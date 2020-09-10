20909c4da6abf691a29d783a84628ff1ab9e9e2a
Mansoor Khalid <62415221+mansoorkhalidgithub@users.noreply.github.com>
2020-09-10 16:44:44 +0500

    Variations Changes

--- app/ItemVariation.php
+++ app/ItemVariation.php
@@ -29,13 +29,16 @@ class ItemVariation extends Model
 
 	public function getDrinksAttribute()
 	{
+		$menuItem = MenuItem::where('id', $this->menu_item_id)->first();
+		$menuCategory = MenuCategory::where('id', $menuItem->menu_category_id)->first();
+		
 		$drinks = [];
 
 		$is_drink = $this->is_drink;
 
-		if ($is_drink == 1) {
+		if ($is_drink == 1 || $menuCategory->type == 'Deal') {
 			$drinks = Drink::all();
-		}
+		} 
 
 		return $drinks;
 	}
